71ad98fda6033f23e873b669ecabd31473af393b
LilTzar <eeoerkeedu@gmail.com>
2025-02-28 13:28:08 -0700

    altered system to be average based instead of simulation based

--- hello.py
+++ hello.py
@@ -1,8 +1,59 @@
-import numpy as np
+hits= 12 
+clash= 4
+reroll= False
+rr6s= False
+relentless= False
+torrential= False
+rr1s=False
 
-from icepool import d
+defence = 1
+cleave = 0
+hardened = 0
+evasion = 0
+deadly = False
+precice = False
+defRR = False
+rr6def = False
+rr1def = False
+shield = False
+lineBreaker = False
+smite = False
+tenacious = False
+bastion = 0
 
-d6 = d(6)
-# print(3 @ d6)
+def defenseCheck(hits, defence):
+	pass
 
-print(3 @ d6.reroll([6], depth=1))
+def hitsCheck(attacks, clash, reroll, rr6s, relentless, torrential, rr1s):
+	hitProb = .166666 * clash
+	hits = attacks * hitProb
+	# if reroll == True and rr6s == True:
+	# 	print("You may not have rr6 and Reroll all dice, please choose one or the other")
+	# 	return
+	if reroll == True:
+		rerolledHits = (attacks - hits) * hitProb
+		hits += rerolledHits
+	if rr6s == True and rr1s == False and reroll == False:
+		rerolledHits = (attacks - hits) * .166666
+		hits += rerolledHits
+	if rr1s == True and rr6s == False and reroll == False:
+		ones = hits * .166666
+		hits -= ones
+		rerolledHits = ones * (.166666 * clash)
+		hits += rerolledHits
+	if relentless == True:
+		extraHits = hits * .166666
+		hits += extraHits
+	if torrential == True:
+		extraHits = hits * .5
+		hits += extraHits
+	
+	print("With a Clash of", clash, "and", attacks, "attacks, you will average", int(hits), "hits!")
+	return hits
+
+attackDict = {"hits": 12, "clash":4, "reroll": False, "rr6s": False, "relentless": False, "torrential": False, "rr1s":False}
+defenseDict = {"defence": 2, "cleave": 0, "hardened": 0, "evasion": 1, "deadly": False, "precice": False, "defRR": False, "rr6def": False, "rr1def": False, "shield": False, "lineBreaker": False, "smite": False, "tenacious": False, "bastion": False}
+
+hitsCheck(hits, clash, reroll, rr6s, relentless, torrential, rr1s)
+
+
