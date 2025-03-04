d02acac8fb915bb228f95260719053bac58c8524
LilTzar <eeoerkeedu@gmail.com>
2025-03-03 17:40:54 -0700

    added defense boosts need to finish most complex parts

--- dice.py
+++ dice.py
@@ -1,4 +1,6 @@
-attacks= 12 
+d6 = .166666
+
+attacks= 25 
 clash= 4
 reroll= False
 rr6s= False
@@ -6,29 +8,55 @@ relentless= False
 torrential= False
 rr1s=False
 
-defence = 3
+defence = 2
 cleave = 0
 hardened = 0
-evasion = 0
+evasion = 1
 deadly = False
 precice = False
 defRR = False
 rr6def = False
-rr1def = False
-shield = False
+rr1def = True
+shield = True
 lineBreaker = False
 smite = False
-tenacious = False
+tenacious = 0
 bastion = 0
 
 def defenseCheck():
 	hits = hitsCheck()
-	defProb = .166666 * defence
+	modifiedDefence = defence
+	if shield == True and lineBreaker == False:
+		modifiedDefence += 1
+	if bastion > 0 and lineBreaker == False:
+		modifiedDefence += bastion
+	if hardened > 0 or cleave > 0:
+		cvh = cleave - hardened
+		if cvh < 0: cvh = 0
+		modifiedDefence -= cvh
+	if evasion > modifiedDefence or smite == True:
+		defProb = d6 * evasion
+		wounds = hits - (hits * defProb)
+		print("With an Evasion of", evasion, "you will average", int(wounds), "wounds!")
+		return wounds
+
+	defProb = d6 * modifiedDefence
 	wounds = hits - (hits * defProb)
-	print("With a Defense of", defence, "you will average", int(wounds), "wounds!")
+	if defRR == True:
+		rerolledSaves = wounds * defProb
+		wounds -= rerolledSaves
+	if rr6def == True and defRR == False and rr1def == False:
+		sixes = wounds * d6
+		wounds -= sixes
+	if rr1def == True and defRR == False and rr6def == False:
+		ones = wounds * d6
+		wounds += ones
+
+	print("With a Defense of", modifiedDefence, "you will average", int(wounds), "wounds!")
+	return wounds
 
 def hitsCheck():
-	hitProb = .166666 * clash
+	hitProb = d6 * clash
 	hits = attacks * hitProb
 	# if reroll == True and rr6s == True:
 	# 	print("You may not have rr6 and Reroll all dice, please choose one or the other")
@@ -37,15 +65,15 @@ def hitsCheck():
 		rerolledHits = (attacks - hits) * hitProb
 		hits += rerolledHits
 	if rr6s == True and rr1s == False and reroll == False:
-		rerolledHits = (attacks - hits) * .166666
+		rerolledHits = (attacks - hits) * d6
 		hits += rerolledHits
 	if rr1s == True and rr6s == False and reroll == False:
-		ones = hits * .166666
+		ones = hits * d6
 		hits -= ones
-		rerolledHits = ones * (.166666 * clash)
+		rerolledHits = ones * (d6 * clash)
 		hits += rerolledHits
 	if relentless == True:
-		extraHits = hits * .166666
+		extraHits = hits * d6
 		hits += extraHits
 	if torrential == True:
 		extraHits = hits * .5
