a27c45d469c24232bbeb18279c2112572dba9516
LilTzar <eeoerkeedu@gmail.com>
2025-03-04 19:11:46 -0700

    added resolve damage section

--- dice.py
+++ dice.py
@@ -10,7 +10,7 @@ relentless = False
 torrential = False
 rr1s = False
 
-# Defnesive modifiers
+# Defenesive modifiers
 defence = 2
 cleave = 0
 hardened = 0
@@ -25,6 +25,52 @@ lineBreaker = False
 smite = False
 tenacious = 1
 bastion = 0
+
+# Morale Modifiers
+ignoresResolves = True
+resolve = 3
+Terrifying = 0
+Fearless = False
+indomitable = 0
+rrSuccess = False
+rrResolve = False
+rr6Res = False
+rr1Res = False
+oblivious = False
+
+def moraleCheck():
+	wounds = defenseCheck()
+	modifiedResolve = resolve
+	if ignoresResolves == True:
+		print("Target unit ignores resolve, so no further casualties are suffered")
+		return casualties
+	if Terrifying > 0 and Fearless == False:
+		modifiedResolve -= Terrifying
+	
+	passResolve = wounds * (d6 * resolve)
+	casualties = wounds - passResolve
+
+	if rrResolve == True and rrSuccess == False:
+		rerolledRes = casualties * passResolve
+		casualties -= rerolledRes
+	if rr6Res == True and rrResolve == False and rr1Res == False and rrSuccess == False:
+		sixes = casualties * d6
+		casualties -= sixes
+	if rr1Res == True and rrResolve == False and rr6Res == False and rrSuccess == False:
+		ones = casualties * d6
+		casualties += ones
+	if rrResolve == False and rrSuccess == True:
+		rerolledRes = (wounds - casualties) * passResolve
+		casualties += rerolledRes
+	if indomitable > 0:
+		casualties -= indomitable
+	if oblivious == True:
+		casualties = casualties * .5
+
+	print("With a Resolve of", resolve, "you will average", int(casualties), "additional casualties!")
+	return casualties
+
+
 
 def defenseCheck():
 	hits = hitsCheck()
@@ -99,9 +145,6 @@ def hitsCheck():
 	print("With a Clash of", clash, "and", attacks, "attacks, you will average", int(hits), "hits!")
 	return hits
 
-attackDict = {"hits": 12, "clash":4, "reroll": False, "rr6s": False, "relentless": False, "torrential": False, "rr1s":False}
-defenseDict = {"defence": 2, "cleave": 0, "hardened": 0, "evasion": 1, "deadly": False, "precice": False, "defRR": False, "rr6def": False, "rr1def": False, "shield": False, "lineBreaker": False, "smite": False, "tenacious": False, "bastion": False}
-
-defenseCheck()
+moraleCheck()
 
 
