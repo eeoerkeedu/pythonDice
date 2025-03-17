5a48c24f44dbe7061697b9e980ebf6af447b1626
LilTzar <eeoerkeedu@gmail.com>
2025-03-17 16:51:02 -0600

    walked back user model changes to focus on in browswer development

--- hello/models.py
+++ hello/models.py
@@ -1,37 +1,37 @@
 from django.db import models
-# from django.utils import timezone
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from .mangers import CustomUserManager
 
-# class LogMessage(models.Model):
-#     message = models.CharField(max_length=300)
-#     log_date = models.DateTimeField("date logged")
-
-#     def __str__(self):
-#         """Returns a string representation of a message."""
-#         date = timezone.localtime(self.log_date)
-#         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
-
-class CustomUser(AbstractUser):
-    username = None
-    first_name = None
-    last_name = None
-    is_staff = None
-    is_superuser = None
-    email = models.EmailField(_("email address"), unique=True)
-    savedProfiles = models.ForeignKey(
-        "Profiles",
-        on_delete=models.CASCADE,
-    )
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
+class LogMessage(models.Model):
+    message = models.CharField(max_length=300)
+    log_date = models.DateTimeField("date logged")
 
     def __str__(self):
-        return self.email
+        """Returns a string representation of a message."""
+        date = timezone.localtime(self.log_date)
+        return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
+
+# class CustomUser(AbstractUser):
+#     username = None
+#     first_name = None
+#     last_name = None
+#     is_staff = None
+#     is_superuser = None
+#     email = models.EmailField(_("email address"), unique=True)
+#     savedProfiles = models.ForeignKey(
+#         "Profiles",
+#         on_delete=models.CASCADE,
+#     )
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = []
+
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.email
     
-class unitProfile():
-    pass
+# class unitProfile():
+#     pass
--- web_project/settings.py
+++ web_project/settings.py
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'static_collected'
 
-AUTH_USER_MODEL = "users.CustomUser"
+# AUTH_USER_MODEL = "hello.CustomUser"
 
